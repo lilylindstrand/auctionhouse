c211dce40b279ac5bf7b923088435b055e094b8b
hepno <me@hepno.dev>
2024-02-28 14:01:11 +0100

    feat: getter/setter methods for databasemanager

---
+++ src/main/java/com/lilylindstrand/auctionhouse/ItemSerializer.java
@@ -0,0 +1,32 @@
+package com.lilylindstrand.auctionhouse;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class ItemSerializer {
+
+    public static @NotNull String encode(@Nullable ItemStack item) {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+             BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream)) {
+            dataOutput.writeObject(item);
+            return new String(Base64Coder.encode(outputStream.toByteArray()));
+        }
+        catch (Exception e) { throw new RuntimeException(e); }
+    }
+
+    public static @Nullable ItemStack decode(@NotNull String base64) {
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decode(base64));
+             BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream)) {
+            return (ItemStack) dataInput.readObject();
+        }
+        catch (Exception e) { throw new RuntimeException(e); }
+    }
+
+}
--- src/main/java/com/lilylindstrand/auctionhouse/manager/DatabaseManager.java
+++ src/main/java/com/lilylindstrand/auctionhouse/manager/DatabaseManager.java
@@ -1,8 +1,12 @@
 package com.lilylindstrand.auctionhouse.manager;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.lilylindstrand.auctionhouse.ItemSerializer;
+import org.bukkit.inventory.ItemStack;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class DatabaseManager {
 
@@ -14,9 +18,11 @@ public class DatabaseManager {
 
     private Connection connection;
 
+    /* Methods */
     public void connect() {
         try { connection = DriverManager.getConnection("jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE + "?useSSL=false", USERNAME, PASSWORD); }
         catch (SQLException e) { e.printStackTrace(); }
+        setupDatabase();
     }
 
     public void disconnect() {
@@ -28,6 +34,81 @@ public class DatabaseManager {
 
     public boolean isConnected() {
         return connection != null;
+    }
+
+    public void setupDatabase() {
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS auctionhouse (" +
+                            "item VARCHAR(255) PRIMARY KEY," +
+                            "selleruuid CHAR(36)," +
+                            "price INT," +
+                            "upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                            ")"
+            );
+            statement.executeUpdate();
+        }
+        catch (SQLException e) { throw new RuntimeException(e); }
+    }
+
+    /* Getters & Setters */
+    public void listItem(String base64Item, UUID sellerUUID, int price) {
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO auctionhouse (item, selleruuid, price) VALUES (?, ?, ?)"
+            );
+            statement.setString(1, base64Item);
+            statement.setString(2, sellerUUID.toString());
+            statement.setInt(3, price);
+
+            statement.executeUpdate();
+        }
+        catch (SQLException e) { throw new RuntimeException(e); }
+    }
+
+    public List<ItemStack> getAllItems() {
+        List<ItemStack> itemStacks = new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT item FROM auctionhouse"
+            );
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                itemStacks.add(ItemSerializer.decode(resultSet.getString("item")));
+            }
+            return itemStacks;
+        }
+        catch (SQLException e) { throw new RuntimeException(e); }
+    }
+
+    public int getItemPrice(String base64Item) {
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT price FROM auctionhouse WHERE item = ?"
+            );
+            statement.setString(1, base64Item);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt("price");
+            }
+        }
+        catch (SQLException e) { throw new RuntimeException(e); }
+        return 0;
+    }
+
+    public UUID getItemSeller(String base64Item) {
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT selleruuid FROM auctionhouse WHERE item = ?"
+            );
+            statement.setString(1, base64Item);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return UUID.fromString(resultSet.getString("selleruuid"));
+            }
+        }
+        catch (SQLException e) { throw new RuntimeException(e); }
+        return null;
     }
 
 }
