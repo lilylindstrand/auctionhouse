f317b2794ac2a9b515abfdc160c93f0f454f1d09
hepno <me@hepno.dev>
2024-02-28 13:11:04 +0100

    feat: getters in configmanager

--- src/main/java/com/lilylindstrand/auctionhouse/manager/ConfigManager.java
+++ src/main/java/com/lilylindstrand/auctionhouse/manager/ConfigManager.java
@@ -15,11 +15,12 @@ public class ConfigManager {
     File dbConfigFile;
     FileConfiguration dbConfiguration;
 
+    /* Constructor */
     public ConfigManager(AuctionHouse plugin) {
         this.plugin = plugin;
     }
 
-
+    /* Methods */
     public void setupConfig() throws IOException, InvalidConfigurationException {
         plugin.saveDefaultConfig();
         fileConfiguration = plugin.getConfig();
@@ -32,6 +33,31 @@ public class ConfigManager {
 
         dbConfiguration = new YamlConfiguration();
         dbConfiguration.load(dbConfigFile);
+        saveConfigData();
     }
+
+    // Saves config data after loading the file, so that files only have to be accessed once, not every time the data is used.
+    // TODO: /reload command
+    String dbHost;
+    int dbPort;
+    String dbDatabase;
+    String dbUsername;
+    String dbPassword;
+    public void saveConfigData() {
+        dbHost = dbConfiguration.getString("host");
+        dbPort = dbConfiguration.getInt("port");
+        dbDatabase = dbConfiguration.getString("database");
+        dbUsername = dbConfiguration.getString("database");
+        dbPassword = dbConfiguration.getString("database");
+    }
+
+    /* Getters */
+    public String getDbHost() { return dbHost; }
+    public int getDbPort() { return dbPort; }
+    public String getDbDatabase() { return dbDatabase; }
+    public String getDbUsername() { return dbUsername; }
+    public String getDbPassword() { return dbPassword; }
+
+
 
 }
