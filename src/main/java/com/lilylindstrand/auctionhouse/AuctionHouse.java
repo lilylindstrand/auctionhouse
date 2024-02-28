28205648435334f818b4f92c730c221de8c650da
hepno <me@hepno.dev>
2024-02-28 12:05:29 +0100

    add(lib): vault

--- src/main/java/com/lilylindstrand/auctionhouse/AuctionHouse.java
+++ src/main/java/com/lilylindstrand/auctionhouse/AuctionHouse.java
@@ -1,12 +1,23 @@
 package com.lilylindstrand.auctionhouse;
 
 import com.lilylindstrand.auctionhouse.command.Ah;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AuctionHouse extends JavaPlugin {
 
+    private static Economy economy = null;
+
     @Override
     public void onEnable() {
+        if (!setupEconomy()) {
+            getLogger().severe("Missing dependency: Vault. Disabling...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
         registerCommands();
     }
 
@@ -17,5 +28,15 @@ public final class AuctionHouse extends JavaPlugin {
 
     public void registerCommands() {
         new Ah("ah", "auctionhouse.use", new String[]{"auctionhouse", "auction"}, "");
+    }
+
+    public boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> registeredServiceProvider = getServer().getServicesManager().getRegistration(Economy.class);
+        if (registeredServiceProvider == null) { return false; }
+        economy = registeredServiceProvider.getProvider();
+        return economy != null;
     }
 }
--- src/main/java/com/lilylindstrand/auctionhouse/command/Ah.java
+++ src/main/java/com/lilylindstrand/auctionhouse/command/Ah.java
@@ -7,7 +7,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
